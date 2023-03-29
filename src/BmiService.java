d90ac3139476e59cd8dd0538c8688f699f3ef787
EkaterinaZak612 <126900017+EkaterinaZak612@users.noreply.github.com>
2023-03-29 12:33:00 +0200

    Update BmiService.java

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,5 +1,5 @@
 public class BmiService {
-    public int calculate(double height) {
+    public int calculate(double height, double weight) {
         int weight = 98;
         double index = (double) (height + height);
         int bmi = (int) (weight / index);
